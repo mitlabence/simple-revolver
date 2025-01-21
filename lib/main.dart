7712561a7f8fff44bf72874bd58bcb7f982a7336
Bence Mitlasoczki <mitlabence111@gmail.com>
2025-01-21 22:00:04 +0100

    Add icon, add/reset revolvers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:just_audio/just_audio.dart';
 
 const kNChambers = 6; // the number of chambers the revolver should have
 const kLoadedChamberColor = Colors.red;
@@ -17,11 +21,68 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int nPlayers = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Revolver"),
+        actions: [
+          PopupMenuButton<String>(
+            itemBuilder: (BuildContext) => <PopupMenuEntry<String>>[
+              const PopupMenuItem<String>(
+                value: "reset_view",
+                child: const Text("Reset"),
+              )
+            ],
+            onSelected: (String result) {
+              if (result == "reset_view") {
+                setState(() {
+                  nPlayers = 1;
+                });
+              }
+            },
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              nPlayers++;
+            });
+          }),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: List.generate(
+              nPlayers, (val) => RevolverCardWidget(key: Key(val.toString()))),
+        ),
+      ),
+    );
+  }
+}
+
+class RevolverCardWidget extends StatefulWidget {
+  const RevolverCardWidget({super.key});
+
+  @override
+  State<RevolverCardWidget> createState() => _RevolverCardWidgetState();
+}
+
+class _RevolverCardWidgetState extends State<RevolverCardWidget> {
   int currentChamber = 0;
   late List<Color> shotColors;
   bool isFiringEnabled = true;
   late int loadedChamberIndex;
-  var rng;
+  late AudioPlayer _audioPlayer;
+  late Random rng;
+  final buttonStyle = ElevatedButton.styleFrom(
+    foregroundColor: Colors.blue,
+    backgroundColor: Colors.white,
+  );
 
   @override
   void initState() {
@@ -29,75 +90,103 @@ class _MainScreenState extends State<MainScreen> {
     shotColors = List.filled(kNChambers, Colors.black);
     rng = Random();
     loadedChamberIndex = rng.nextInt(kNChambers);
+    _audioPlayer = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final buttonStyle = ElevatedButton.styleFrom(
-      foregroundColor: Colors.blue,
-      backgroundColor: Colors.white,
-    );
-    return Scaffold(
-        appBar: AppBar(title: const Text("Revolver")),
-        body: Center(
-          child: Column(
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                final int newLoadedChamberIndex = rng.nextInt(kNChambers);
+                setState(() {
+                  isFiringEnabled = false;
+                  loadedChamberIndex = newLoadedChamberIndex;
+                  shotColors = List.filled(kNChambers, Colors.black);
+                  currentChamber = 0;
+                });
+                await _audioPlayer.setAsset("assets/audio/revolver_spin.mp3");
+                await _audioPlayer.play();
+                setState(() {
+                  isFiringEnabled = true;
+                });
+              },
+              style: buttonStyle,
+              child: const Text("Load revolver"),
+            ),
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                    kNChambers,
-                    (int i) => Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: shotColors[i],
-                          shape: BoxShape.circle,
-                        ))),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    final int newLoadedChamberIndex = rng.nextInt(kNChambers);
-                    setState(() {
-                      loadedChamberIndex = newLoadedChamberIndex;
-                      shotColors = List.filled(kNChambers, Colors.black);
-                      currentChamber = 0;
-                      isFiringEnabled = true;
-                    });
-                  },
-                  child: const Text("Load revolver"),
-                  style: buttonStyle),
-              ElevatedButton(
-                onPressed: isFiringEnabled
-                    ? () async {
-                        // fire current chamber. If it was loaded, game over; else
-                        // increment current chamber. Lock the fire button for one
-                        // second
-                        if (currentChamber == loadedChamberIndex) {
-                          // set corresponding circle to "loadedChamberColor"
-                          // lock fire button, only leave reset button
-                          setState(() {
-                            shotColors[currentChamber] = kLoadedChamberColor;
-                            isFiringEnabled = true;
-                          });
-                        }
-                        else { // empty chamber
-                          setState(() {
-                            shotColors[currentChamber] = kEmptyChamberColor;
-                            currentChamber++;
-                          });
-                        }
-                        // TODO: in if cases above, select audio, then play
-                        // it here. Then wait for it to play (or wait 1 s)
-                      }
-                    : null,
-                child: const Text("Fire"),
-                style: buttonStyle,
-              ),
-            ],
+            children: List.generate(
+                kNChambers,
+                (int i) => Padding(
+                      padding: const EdgeInsets.all(1.0),
+                      child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: shotColors[i],
+                            shape: BoxShape.circle,
+                          )),
+                    )),
           ),
-        ));
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: isFiringEnabled
+                  ? () async {
+// fire current chamber. If it was loaded, game over; else
+// increment current chamber. Lock the fire button for one
+// second
+                      String assetPath;
+                      MaterialColor shotColor;
+                      bool allowFiringAfterCurrentShot;
+                      setState(() {
+                        isFiringEnabled = false;
+                      });
+                      if (currentChamber == loadedChamberIndex) {
+// set corresponding circle to "loadedChamberColor"
+// lock fire button, only leave reset button
+                        assetPath = 'assets/audio/revolver_shot.mp3';
+                        shotColor = kLoadedChamberColor;
+                        allowFiringAfterCurrentShot =
+                            false; // only reload should work
+                      } else {
+// empty chamber
+                        assetPath = 'assets/audio/revolver_empty.mp3';
+                        shotColor = kEmptyChamberColor;
+                        allowFiringAfterCurrentShot = true;
+                      }
+                      await _audioPlayer.setAsset(assetPath);
+                      _audioPlayer.play();
+                      Future.delayed(Duration(seconds: 1), () {
+                        setState(() {
+                          shotColors[currentChamber] = shotColor;
+                          isFiringEnabled = allowFiringAfterCurrentShot;
+                          currentChamber++;
+                        });
+                      });
+                    }
+                  : null,
+              style: buttonStyle,
+              child: const Text("Fire"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
